cf43d54bbe441b2672191f3e6d45de8e0f24e073
PamLeles <pamela_leles_sousa@outlook.com>
2022-09-14 18:07:44 -0300

    Form and table made, page stylization and responsiveness started for better user interaction

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,85 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div class="app">
+      <form class="form">
+        <h1 class="form-title">Formulário de Cadastro</h1>
+        <div class="label-div">
+          <label>
+            CNPJ:
+            <input type="number" id="cnpj" required />
+          </label>
+
+          <label>
+            Nome da empresa:
+            <input type=" text" id="name" required />
+          </label>
+
+          <label>
+            CEP:
+            <input type="number" id="cep" required />
+          </label>
+
+          <label>
+            Endereço:
+            <input type=" text" id="address" required />
+          </label>
+
+          <label>
+            Número:
+            <input type="number" class="input-number" required />
+          </label>
+
+          <label>
+            Bairro:
+            <input type="text" id="district" required />
+          </label>
+          <div class="actions-wrapper">
+            <label class="label-uf-city">
+              UF:
+              <select id="select-uf">
+                <option value="" disabled selected>
+                  Insira o CEP
+                </option>
+              </select>
+            </label>
+
+            <label class="label-uf-city">
+              cidade:
+              <select id="select-city">
+                <option value="" disabled selected>
+                  Insira o CEP
+                </option>
+              </select>
+            </label>
+          </div>
+
+          <div class="actions-wrapper">
+            <button class="btn-cancel">Limpar</button>
+            <button class="btn-save" onclick="saveForm(event)">
+              Salvar
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <section>
+        <h2>Empresas Cadastradas:</h2>
+
+        <table>
+          <thead>
+            <tr>
+              <th>CNPJ</th>
+              <th>Nome da Empresa</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody id="table-content"></tbody>
+        </table>
+      </section>
+    </div>
+  );
 }
 
 export default App;
